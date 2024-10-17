74a5022584dd2c4b90d8ee924f56bf82e66659b7
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-10-16 22:44:17 -0500

    save work'

--- ASE 367K/hw5p4.m
+++ ASE 367K/hw5p4.m
@@ -8,3 +8,25 @@ omegaB     = [p;q;r];
 hB        = [hx; hy; hz];
 
 MB = IB*omegaBdot + omegatilde*(IB*omegaB+hB)
+
+%% HW 5 p3a)
+clear all;
+close all;
+clc; 
+
+cT   = 3.522*10^-5; % kg/(N*s)
+S    = 125; % m^2
+k    = 0.048;
+Cd0  =  0.016;
+mi   = 78000; % initial mass kg
+mf   = 58000; % final mass kg
+rho  = 0.380349568; % air density at 35000 ft. in kg/m^3
+g    = 9.7737168; % Gravity Acceleration at 35000 ft. in m/s^2
+a    = 295.046; % Speed of sound at 35000 ft. in m/s; constant at 35000 ft
+Mach = 0.8; % Given
+v    = a*Mach; % V
+
+fun   = @(m) 1./(0.5*rho*v*S*Cd0+(k*m.^2*g^2)/(2*rho*v^3*S))*1./-cT;
+range = integral (fun,mi,mf)
+
+
