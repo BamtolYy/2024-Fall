c56f317077bf54f50d701eb8501ba248d247ff44
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-10-23 23:13:33 -0500

    hw6 first draft

--- ASE 367K/hw6p1.m
+++ ASE 367K/hw6p1.m
@@ -1,3 +1,7 @@
+clear all;
+close all;
+clc;
+
 Xu     =-0.0188;
 XTu    = 0;
 Xa     = 11.5905;
@@ -74,32 +78,63 @@ N = abs(log(2)*wd/(2*pi*real(eVal(1,1))));
 % delT = log(2)/(damp*wn);
 % N = log(2)*wd/(2*pi*damp*wn);
 
+
 % Long
 wdLong   = imag(eVal(3,3));
 wnLong   = sqrt((real(eVal(3,3)))^2+(wdLong)^2);
 dampLong = abs(real(eVal(3,3))/wnLong);
 delTLong = abs(log(2)/(real(eVal(3,3))));
 NLong = abs(log(2)*wdLong/(2*pi*real(eVal(3,3))));
+
 %% b)
 x0 = zeros(4,1);
 sys = ss(A,B,C,D);
+[h, t] = impulse(sys);
 figure(1)
-impulse(sys)
+sgtitle('Impulse Response of Linear Longitudinal Dynamics')
+subplot(4,1,1)
+plot(t,h(:,1))
+ylabel('\Delta u [ft/s]');
+xlabel('Time [seconds]')
+subplot(4,1,2)
+plot(t,h(:,2))
+ylabel('\Delta  \alpha [rad^-1]');
+xlabel('Time [seconds]')
+subplot(4,1,3)
+plot(t,h(:,3))
+ylabel('\Delta q [rad/sec] ');
+xlabel('Time [seconds]')
+subplot(4,1,4)
+plot(t,h(:,4))
+ylabel('\Delta \theta [ft/s]');
+xlabel('Time [seconds]')
+
+[hs, ts] = step(sys);
 figure(2)
-step(sys)
-% cdt = 0.01;
-% ct= [0:0.01:2000];
-% 
-% for i = 1:ct
+sgtitle('Step Response of Linear Longitudinal Dynamics')
+subplot(4,1,1)
+plot(ts,hs(:,1))
+ylabel('\Delta u [ft/s]');
+xlabel('Time [seconds]')
+subplot(4,1,2)
+plot(ts,hs(:,2))
+ylabel('\Delta  \alpha [rad^-1]');
+xlabel('Time [seconds]')
+subplot(4,1,3)
+plot(ts,hs(:,3))
+ylabel('\Delta q [rad/sec] ');
+xlabel('Time [seconds]')
+subplot(4,1,4)
+plot(ts,hs(:,4))
+ylabel('\Delta \theta [ft/s]');
+xlabel('Time [seconds]')
 
 %% Problem 2
-
 
 M2 = [u1 0;
     -Madot 1];
 R2 = [Za u1;
     Ma Mq];
-
 A2 = inv(M2)*R2;
 
 [eVec2,eVal2] = eig(A2);
@@ -122,32 +157,33 @@ D2 = zeros(2,1);
 
 sys2 = ss(A2,B2,C2,D2);
 
-figure(3),
-step(sys2);
-figure (4),
-impulse(sys2);
+[h2, t2] = impulse(sys2);
+figure(3)
+sgtitle('Impulse Response of Short Period Mode')
+subplot(2,1,1)
+plot(t2,h2(:,1))
+ylabel('\Delta \alpha [rad^-1]');
+xlabel('Time [seconds]')
+subplot(2,1,2)
+plot(t2,h2(:,2))
+ylabel('\Delta  q [rad/sec]');
+xlabel('Time [seconds]')
+
+[h2s, t2s] = step(sys2);
+figure(4)
+sgtitle('Step Response of Short Period Mode')
+subplot(2,1,1)
+plot(t2s,h2s(:,1))
+ylabel('\Delta \alpha [rad^-1]');
+xlabel('Time [seconds]')
+subplot(2,1,2)
+plot(t2s,h2s(:,2))
+ylabel('\Delta  q [rad/sec]');
+xlabel('Time [seconds]')
+
 
 %% Problem 3
 % a)
-% A3 = [Xu+XTu -g;
-%     -Zu/u1 0];
-% F3 = [Xde;
-%     0];
-% B3 = F3;
-% C3 = eye(2);
-% D3 = zeros(2,1);
-% 
-% sys3 = ss(A3,B3,C3,D3);
-% 
-% figure(5),
-% dt = 0.01;
-% t = [0:dt:1000];
-% 
-% u = ones(length(t),1);
-% x0 = [0,1];
-% figure(5);
-% lsim(sys3,u,t,x0)
-
 A3 = [Xu+XTu -g;
     -Zu/u1 0];
 F3 = [Xde;
@@ -156,6 +192,73 @@ B3 = F3;
 C3 = eye(2);
 D3 = 0;
 
+[eVec3,eVal3] = eig(A3);
+
+wd3   = imag(eVal3(1,1));
+wn3   = sqrt((real(eVal3(1,1)))^2+(wd3)^2);
+damp3 = abs(real(eVal3(1,1))/wn2);
+delT3 = abs(log(2)/(real(eVal3(1,1))));
+N3    = abs(log(2)*wd3/(2*pi*real(eVal3(1,1))));
+
+
 sys3 = ss(A3,B3,C3,D3);
 
-impulse(sys3)
+[h3, t3] = impulse(sys3);
+figure(5)
+sgtitle('Impulse Response of Phugoid(long period) Mode')
+subplot(2,1,1)
+plot(t3,h3(:,1))
+ylabel('\Delta u [rad^-1]');
+xlabel('Time [seconds]')
+subplot(2,1,2)
+plot(t3,h3(:,2))
+ylabel('\Delta  \theta [rad/sec]');
+xlabel('Time [seconds]')
+
+[h3s, t3s] = step(sys3);
+figure(6)
+sgtitle('Step Response of Phugoid(long period) Mode')
+subplot(2,1,1)
+plot(t3s,h3s(:,1))
+ylabel('\Delta u [rad^-1]');
+xlabel('Time [seconds]')
+subplot(2,1,2)
+plot(t3s,h3s(:,2))
+ylabel('\Delta  \theta [rad/sec]');
+xlabel('Time [seconds]')
+
+
+%% Print Answers
+fprintf('\nNumber 1 \n');
+fprintf('Short Period\n');
+fprintf('---------------------------\n');
+fprintf('Damped Frequency: %f [rad/s]\n', wd);
+fprintf('Natural Frequency: %f [rad/s]\n', wn);
+fprintf('Damping Ratio: %f \n', damp);
+fprintf('Time to Damp to Half the initial Amplitude: %f [sec]\n', delT);
+fprintf('Cycle to Damp to Half the initial Amplitude: %f [cycle]\n', N);
+fprintf('\nLong Period\n');
+fprintf('---------------------------\n');
+fprintf('Damped Frequency: %f [rad/s]\n', wdLong);
+fprintf('Natural Frequency: %f [rad/s]\n', wnLong);
+fprintf('Damping Ratio: %f \n', dampLong);
+fprintf('Time to damp to half the initial Amplitude: %f [sec]\n', delTLong);
+fprintf('Cycle to damp to half the initial Amplitude: %f [cycle]\n', NLong);
+
+fprintf('\nNumber 2 \n');
+fprintf('---------------------------\n');
+fprintf('Damped Frequency: %f [rad/s]\n', wd2);
+fprintf('Natural Frequency: %f [rad/s]\n', wn2);
+fprintf('Damping Ratio: %f \n', damp2);
+fprintf('Time to Damp to Half the initial Amplitude: %f [sec]\n', delT2);
+fprintf('Cycle to Damp to Half the initial Amplitude: %f [cycle]\n', N2);
+
+
+
+fprintf('\nNumber 3 \n');
+fprintf('---------------------------\n');
+fprintf('Damped Frequency: %f [rad/s]\n', wd3);
+fprintf('Natural Frequency: %f [rad/s]\n', wn3);
+fprintf('Damping Ratio: %f \n', damp3);
+fprintf('Time to Damp to Half the initial Amplitude: %f [sec]\n', delT3);
+fprintf('Cycle to Damp to Half the initial Amplitude: %f [cycle]\n', N3);
