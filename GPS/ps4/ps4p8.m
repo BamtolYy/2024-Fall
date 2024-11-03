d3c9b713b13b7c104848bcb28523e7e7df7b61a6
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-11-03 02:36:17 -0600

    save progress

--- GPS/ps4/ps4p8.m
+++ GPS/ps4/ps4p8.m
@@ -1,5 +1,6 @@
 clear; close all; clc;
 %% Get Signal
+
 %----- Setup
 Tfull = 0.5;
 % Time interval of data to load
@@ -7,24 +8,50 @@ fsampIQ = 5.0e6;
 % IQ sampling frequency (Hz)
 N = floor(fsampIQ*Tfull);
 nfft = 2^9;
+fIF     = 2.5e6;         % Intermediate frequency (Hz)
 % Size of FFT used in power spectrum estimation
 %----- Load data
 fid = fopen('C:\Users\gsh04\Desktop\2024-Fall\GPS\ps4\niData01head_5MHz.bin','r','l');
 Y = fread(fid, [2,N], 'int16')';
 Y = Y(:,1) + 1j*Y(:,2);
 fclose(fid);
+%---- Convert IQ to IF
+Tl = 1/fsampIQ;
+[Y] = iq2if(real(Y),imag(Y),Tl,fIF);
 
-%% Problem 8 a) PRN Identifier
+%% Problem 8
 
-% Generate all possible PRN
+%---- Generate all possible PRN (37 SVIDs or PRN Sign No.)
+% LFSR Parameters:
 nStages      = 10;
 ciVec1       = [10, 3]';
 ciVec2       = [10, 9, 8, 6, 3, 2,]';
 a0Vec1       = ones(nStages,1);
 a0Vec2       = ones(nStages,1);
 G2Delay      = [5;6;7;8;17;18;139;140;141;251;252;254;255;256;257;258;...
-                469;470;471;472;473;474;509;512;513;514;515;516;859;860;...
-                861;862;863;950;947;948;950];
+    469;470;471;472;473;474;509;512;513;514;515;516;859;860;...
+    861;862;863;950;947;948;950];
+% Oversampling Parameters:
+Tc = 1e-3/1023;               % Chip interval in seconds
+delChip = 1/Tc*1/fsampIQ;     % Sampling interval in chips
+delOffset  = 0;               % Offset of first sample
+delt = 1/fsampIQ;             % Sampling interval in seconds;
+Np = 2^nStages - 1;           % Period of the sequence in chips
+Ns = length(Y);               % Number of Samples should equal to that of Y(signal)
+% Generate 37 Seqeuences and Oversample them:
+for i = 1:length(G2Delay)
+    [GoldSeq] = generateGoldLfsrSequenceCA(nStages,ciVec1,ciVec2,a0Vec1,...
+        a0Vec2,G2Delay(i));
+    % Make code +1/-1 not +1/0
+    GoldSeq = 2*GoldSeq - 1;
+    % Oversample Code: It makes sense to oversample code, since the code
+    % embedded within the signal is sampled at a higher rate than its chip
+    % rate. Assuming that the code I generate is sampled at the chip rate,
+    % oversampling my code I generated at the rate the signal is sampled
+    % will allow my code to correlate with the code embedded in the signal
+    GoldSeqOS = oversampleSpreadingCode(GoldSeq,delChip,delOffset,Ns,Np);
+    codeOS(:,i) = GoldSeqOS;
+end
 
 %--------------------------------------------------------------------------
 % % Check if two lfsr m-sequence are truly Golden
@@ -51,37 +78,26 @@ G2Delay      = [5;6;7;8;17;18;139;140;141;251;252;254;255;256;257;258;...
 % % crosscorrelation values, they do make up gold codes.
 %--------------------------------------------------------------------------
 
-% Generate Gold Sequences for all 37 SVIDs or PRN Sign No.
-% Oversampling Parameters:
-Tc = 1e-3/1023;               % Chip interval in seconds
-delChip = 1/(Tc*fsampIQ);      % Sampling interval in chips
-delOffset  = 0;               % Offset of first sample
-delt = 1/fsampIQ;             % Sampling interval in seconds;
-Np = 2^nStages - 1;           % Period of the sequence in chips
-Ns = length(Y);               % Number of Samples should equal to that of Y(signal)
-for i = 1:length(G2Delay)
-    [GoldSeq] = generateGoldLfsrSequenceCA(nStages,ciVec1,ciVec2,a0Vec1,...
-        a0Vec2,G2Delay(i));
-    % Make code +1/-1 not +1/0
-    GoldSeq = 2*GoldSeq - 1;
-    % Oversample Code: It makes sense to oversample code, since the code
-    % embedded within the signal is sampled at a higher rate than its chip
-    % rate. Assuming that the code I generate is sampled at the chip rate,
-    % oversampling my code I generated at the rate the signal is sampled
-    % will allow my code to correlate with the code embedded in the signal
-    GoldSeqOS = oversampleSpreadingCode(GoldSeq,delChip,delOffset,Ns,Np);
-    codeOS(:,i) = GoldSeqOS;
-end
+%----
+% Parameters:
+% Potential Doppler Frequencies
+fD = -5000:100:5000;
+% Sampling time interval in seconds
+T = 1/fsampIQ;
+% Nk is the number of samples is one 1-ms accumulation.  It's ok for this
+% number to be approximate
+Nk = 5714;
+% Time vector covering the accumulation
+tVec = [0:Nk-1]'*T;
+%---- 
+for ii = 1:length(fD)
+    %---- Generate local carrier Replica
 
-% Correlate code with Signal
-period = round(Np/delChip);
-for j = 1:size(codeOS,2)
-    [R,iiVec] = ccorr(Y,codeOS(:,j));
-    corrResult(j)    = abs(max(R));
-    figure,
-    plot(iiVec,R/Ns);
-    grid on;
-    ylabel('R_{X1}');
-    title('X1 and X2 autocorrelation')
-    xlim([-period/2,period/2])
+    % Generate the phase argument of the local carrier replica
+    ThetaVec = [2*pi*(fIF + fD(i))*tVec];
+    % Generate Phase argument of the local carrier replica
+    carrierVec = exp(-i*ThetaVec);
+    % Generate the full local replica, with both code and carrier
+    lVeck = carrierVec.*;
 end
+%--------------------------------------------------------------------------
