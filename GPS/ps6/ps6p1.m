ff8498e85ad39695d5cb135235cc3e740c2965dd
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-11-17 23:23:55 -0600

    save progress

--- GPS/ps6/ps6p1.m
+++ GPS/ps6/ps6p1.m
@@ -3,41 +3,44 @@ close all; clear all; clc
 ensemble = 1000000;
 rho = 4;
 theta = 0;
-nI = rand(ensemble,1);
-nQ = rand(ensemble,1);
+nI = randn(ensemble,1);
+nQ = randn(ensemble,1);
 I = rho*cos(theta)+nI;
 Q = rho*sin(theta)+nQ;
 theta_ML = atan2(Q,I);
 
 %% Theoretical
-% Assume noise variances are 1. 
-% Correlation coefficient between Q and I is zero
-
-w = tan(linspace(-pi/2+0.01,pi/2-0.0,100));
-ftheta = zeros(length(w),1);
+% % Assume noise variances are 1. 
+% % Correlation coefficient between Q and I is zero
+% 
 sigma1 = 1;
 sigma2 = 1;
 muI = rho*cos(theta);
 muQ = rho*sin(theta);
-for ii = 1:length(w)
 ccoef = 0;
-a = sqrt(w(ii)^2/sigma1^2-2*ccoef*w(ii)/(sigma1*sigma2)+1/sigma2^2);
-b = muI*(ii)/sigma1^2-ccoef*(muI+muQ*w(ii))/(sigma1*sigma2)+muQ/sigma2^2;
-c = muI^2/sigma1^2-2*ccoef*muI*muQ/(sigma1*sigma2)+muQ^2/sigma2^2;
-d = exp((b^2-c*a^2)/(2*(1-ccoef^2)*a^2));
+theta2 = linspace(-pi/2 + 0.01, pi/2 - 0.01, 1000); % Avoid exact asymptotes
+ftheta = zeros(length(theta2),1);
 
-muI = rho*cos(theta);
-muQ = rho*sin(theta);
-xmin = -b/sqrt(1-ccoef^2*a);
-xmax = b/sqrt(1-ccoef^2*a);
-phi = integral(@(u) exp(-1/2*u.^2)/sqrt(2*pi),xmin,xmax);
-
-
-% PDF of W=Q/I
-pw = b*d/sqrt(2*pi*sigma1*sigma2*a^3)*phi+sqrt(1-ccoef^2)/(pi*sigma1*sigma2*a^2)*exp(-c/(2*(1-ccoef^2)));
-ftheta(ii) = pw*abs(1+tan(w(ii))^2);
+for ii = 1:length(theta2)
+w(ii)      = tan(theta2(ii));
+pw = ratioPdf(w(ii),sigma1,sigma2,muI,muQ,ccoef);
+ftheta(ii) = pw*abs(1+tan(theta2(ii))^2);
 end
-% figure,
-% histogram(theta_ML)
-% hold on,
-plot(w,ftheta)
+
+
+% Plotting the Simulated and Theoretical Distribution
+figure,
+histogram(theta_ML);
+figure,
+plot(ftheta)
+
+% Plot the theoretical PDF of theta
+% plot(theta2, ftheta, 'r', 'LineWidth', 2);
+% 
+% % Set plot labels and display the plot
+% xlabel('\theta (radians)');
+% ylabel('Probability Density');
+% title('Comparison of Simulated and Theoretical Distributions of \theta_{ML}');
+% legend('Simulated Histogram', 'Theoretical Distribution');
+% grid on;
+% hold off;
---
+++ GPS/ps6/ratioPdf.m
@@ -0,0 +1,13 @@
+function [pw] = ratioPdf(w,sigma1,sigma2,theta1,theta2,ccoef)
+
+
+a = sqrt(w^2/sigma1^2-2*ccoef*w/(sigma1*sigma2)+1/sigma2^2);
+b = theta1*w/sigma1^2-ccoef*(theta1+theta2*w)/(sigma1*sigma2)+theta2/sigma2^2;
+c = theta1^2/sigma1^2-2*ccoef*theta1*theta2/(sigma1*sigma2)+theta2^2/sigma2^2;
+d = exp((b^2-c*a^2)/(2*(1-ccoef^2)*a^2));
+xmin = -b/sqrt((1-ccoef^2)*a);
+xmax = b/sqrt((1-ccoef^2)*a);
+phiu = @(u) 1/sqrt(2*pi)*exp(-1/2*u.^2);
+phiy = phiy_upper -phiy_lower;
+pw = b*d/sqrt(2*pi*sigma1*sigma2*a^3)*phiy+sqrt(1-ccoef^2)/...
+    (pi*sigma1*sigma2*a^2)*exp(-c/(2*(1-ccoef^2)));
