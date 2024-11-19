c838134fc2eb1caf9f5c4b9abfee9f788fc1b08e
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-11-18 22:10:53 -0600

    finish 1b

--- GPS/ps6/ps6p1.m
+++ GPS/ps6/ps6p1.m
@@ -1,4 +1,4 @@
-close all; clear all; clc 
+close all; clear all; clc
 %%
 ensemble = 1000000;
 rho = 4;
@@ -10,40 +10,37 @@ Q = rho*sin(theta)+nQ;
 theta_ML = atan2(Q,I);
 
 %% Theoretical
-% % Assume noise variances are 1. 
+% % Assume noise variances are 1.
 % % Correlation coefficient between Q and I is zero
-% 
+%
 sigma1 = 1;
 sigma2 = 1;
 muI = rho*cos(theta);
 muQ = rho*sin(theta);
 ccoef = 0;
-theta2 = linspace(-pi/2 + 0.01, pi/2 - 0.01, 10000); % Avoid exact asymptotes
+theta2 = linspace(-pi/2, pi/2, 100); % Avoid exact asymptotes
 ftheta = zeros(length(theta2),1);
 
+S = rho*exp(i*theta2);
+Q = imag(S);
+I = real(S);
+W = Q./I;
+
+
 for ii = 1:length(theta2)
-w(ii)      = tan(theta2(ii));
-pw = ratioPdf(w(ii),sigma1,sigma2,muI,muQ,ccoef);
-ftheta(ii) = pw*abs(1+tan(theta2(ii))^2);
+a(ii) = sqrt(W(ii)^2+1);
+b(ii) = rho*W(ii)*sin(theta)+rho*cos(theta);
+c(ii) = rho^2;
+d(ii) = exp((b(ii)^2-c(ii)*a(ii)^2)/(2*a(ii)^2));
+phiu = @(u) 1/sqrt(2*pi)*exp(-1/2*u.^2);
+xmin = -b(ii)/a(ii);
+xmax = b(ii)/a(ii);
+phiy = integral(phiu,xmin,xmax);
+pw(ii)   = b(ii)*d(ii)/(sqrt(2*pi)*a(ii)^3)*phiy+1/(pi*a(ii)^2)*exp(-c(ii)/2);
+
+ftheta(ii) = pw(ii)*(1+W(ii)^2);
 end
-
-
-% Plotting the Simulated and Theoretical Distribution
 figure,
-histogram(theta_ML);
-figure,
-plot(ftheta)
-
-% Plot Histogram of Simulated Data
-figure;
-histogram(theta_ML, 'Normalization', 'pdf', 'DisplayName', 'Simulated Histogram');
-hold on;
-% Plot Theoretical PDF
-plot(theta2, ftheta, 'r-', 'LineWidth', 2, 'DisplayName', 'Theoretical Distribution');
-
-%% Label Plot
-xlabel('\theta (radians)');
-ylabel('Probability Density');
-title('Comparison of Simulated and Theoretical Distributions of \theta_{ML}');
-legend('show');
-hold off;
+histogram(theta_ML,"Normalization","pdf")
+hold on,
+plot(theta2,ftheta)
