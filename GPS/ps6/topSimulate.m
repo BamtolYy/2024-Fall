a062c5c8b43061500efe30436c8e71a8f280509f
Bonsuck Koo <bonsuckkoo@gmail.com>
2024-12-05 00:55:43 -0600

    first draft of ps6

--- GPS/ps6/topSimulate.m
+++ GPS/ps6/topSimulate.m
@@ -28,13 +28,13 @@ for ii = 1:N_sub
     [xkp1,vk] = updatePll(s);
     s.xk=xkp1;
     if ii == 1
-         thetahat(start:endpt)=thetahat(start)+vk*[0:T:Ta-T];
+        thetahat(start:endpt)=thetahat(start)+vk*[0:T:Ta-T];
     else
-    thetahat(start:endpt)=thetahat(start-1)+vk*[0:T:Ta-T];
-    deltaTheta = PhaseHist(start:endpt)-thetahat(start:endpt);
-    deltaThetak = mean(deltaTheta);
-    s.Ip = cos(deltaThetak);
-    s.Qp = sin(deltaThetak);
+        thetahat(start:endpt)=thetahat(start-1)+vk*[0:T:Ta-T];
+        deltaTheta = PhaseHist(start:endpt)-thetahat(start:endpt);
+        deltaThetak = mean(deltaTheta);
+        s.Ip = cos(deltaThetak);
+        s.Qp = sin(deltaThetak);
     end
 end
 figure,
@@ -62,13 +62,13 @@ for ii = 1:N_sub
     [xkp1,vk] = updatePll(s);
     s.xk=xkp1;
     if ii == 1
-         thetahat2(start:endpt)=thetahat2(start)+vk*[0:T:Ta-T];
+        thetahat2(start:endpt)=thetahat2(start)+vk*[0:T:Ta-T];
     else
-    thetahat2(start:endpt)=thetahat2(start-1)+vk*[0:T:Ta-T];
-    deltaTheta2 = PhaseHist2(start:endpt)-thetahat2(start:endpt);
-    deltaThetak2 = mean(deltaTheta2);
-    s.Ip = cos(deltaThetak2);
-    s.Qp = sin(deltaThetak2);
+        thetahat2(start:endpt)=thetahat2(start-1)+vk*[0:T:Ta-T];
+        deltaTheta2 = PhaseHist2(start:endpt)-thetahat2(start:endpt);
+        deltaThetak2 = mean(deltaTheta2);
+        s.Ip = cos(deltaThetak2);
+        s.Qp = sin(deltaThetak2);
     end
 end
 figure,
@@ -85,7 +85,7 @@ fprintf(['When there is a 180 degrees phase transition, the Loop\n' ...
     'the overshoot'])
 
 %% d)
-sigman=0.9;
+sigmaIQ=0.4705;
 PhaseHist3 = 2*pi*fd*t;         % Raw Phase History
 % Average over subinterval Ta
 Nk    = floor(Ta/T);        % Number of samples in an accumulation
@@ -102,13 +102,13 @@ for ii = 1:N_sub
     [xkp1,vk] = updatePll(s);
     s.xk=xkp1;
     if ii == 1
-         thetahat3(start:endpt)=thetahat3(start)+vk*[0:T:Ta-T];
+        thetahat3(start:endpt)=thetahat3(start)+vk*[0:T:Ta-T];
     else
-    thetahat3(start:endpt)=thetahat3(start-1)+vk*[0:T:Ta-T];
-    deltaTheta3 = PhaseHist3(start:endpt)-thetahat3(start:endpt);
-    deltaThetak3 = mean(deltaTheta3);
-    s.Ip = cos(deltaThetak3)+sigman*randn;
-    s.Qp = sin(deltaThetak3)+sigman*randn;
+        thetahat3(start:endpt)=thetahat3(start-1)+vk*[0:T:Ta-T];
+        deltaTheta3 = PhaseHist3(start:endpt)-thetahat3(start:endpt);
+        deltaThetak3 = mean(deltaTheta3);
+        s.Ip = cos(deltaThetak3)+sigmaIQ*randn;
+        s.Qp = sin(deltaThetak3)+sigmaIQ*randn;
     end
 end
 figure,
@@ -117,4 +117,47 @@ legend('True','Estimate')
 xlabel('Time (seconds)')
 ylabel('\theta')
 title('Phase Detection with Noise')
-CN0 = 
+
+%% e)
+
+
+PhaseHist4 = 2*pi*fd*t;         % Raw Phase History
+% Average over subinterval Ta
+Nk    = floor(Ta/T);        % Number of samples in an accumulation
+N_sub   = floor(length(t)/Nk);     % Number of subintervals in the signal
+xk0     = zeros(loopOrder-1,1);
+s.xk    = xk0;
+thetahat4 = zeros(length(t),1);
+deltaThetak4 = 0;
+
+CN0 = 10^(21/10);
+Sk = exp(1i * PhaseHist);
+S_power = mean(abs(Sk).^2);
+sigmaIQ = sqrt((S_power) / (2 * CN0 * Ta + 2));
+
+
+
+s.Ip = cos(deltaThetak4);
+s.Qp = sin(deltaThetak4);
+for ii = 1:N_sub
+    start = (ii-1)*Nk+1;
+    endpt = ii*Nk;
+    [xkp1,vk] = updatePll(s);
+    s.xk=xkp1;
+    if ii == 1
+        thetahat4(start:endpt)=thetahat4(start)+vk*[0:T:Ta-T];
+    else
+        thetahat4(start:endpt)=thetahat4(start-1)+vk*[0:T:Ta-T];
+        deltaTheta4 = PhaseHist4(start:endpt)-thetahat4(start:endpt);
+        deltaThetak4 = mean(deltaTheta4);
+        s.Ip = cos(deltaThetak4)+sigmaIQ*randn;
+        s.Qp = sin(deltaThetak4)+sigmaIQ*randn;
+    end
+end
+figure,
+plot(t,PhaseHist4,t(1:end-1),thetahat4(1:end-1),'--')
+legend('True','Estimate')
+xlabel('Time (seconds)')
+ylabel('\theta')
+title('Phase Detection with Noise')
+sigmaIQ
